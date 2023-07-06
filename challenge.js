43d338074e0a55df3c1806b93426ea563e40a887
Ibtissam <btissam1234btissam12@gmail.com>
2023-07-06 12:23:57 +0100

    number assiegment

--- challenge.js
+++ challenge.js
@@ -1,3 +1,4 @@
+//challenge 1
 let cardTitle="Elzero",cardDescription= "Elzero Web School",cardDate="25/10";
 let content=`
  <div class="parent">
@@ -8,3 +9,40 @@ let content=`
 `;
 let contentRepeatet= content.repeat(4);
 document.write(contentRepeatet);
+//challenge 2
+
+/*
+  Challenge 1
+*/
+
+let a = 10;
+let b = "20";
+let c = 80;
+
+console.log(++a + +b++ + +c++ - +a++);//11 + 20 +80 -12 =99
+console.log(++a + -b + +c++ - -a++ + +a);// 13 -20 +81 - -13 -14 =73
+console.log(--c + +b + --a * +b++ - +b * a + --a - +true);
+
+/*
+  [++a] []
+  [++a]
+  - Value:11
+  - Explain:
+  [+]
+  - Explain:
+*/
+
+/*
+  Challenge 2
+*/
+
+let d = "-100";
+let e = "20";
+let f = 30;
+let g = true;
+
+// Only Use Variables Value
+// Do Not Use Variable Twice
+
+console.log(-d*(+e)); // 2000
+console.log(-d + +e +f + 23*g); // 173
--- main.js
+++ main.js
@@ -56,7 +56,91 @@ console.log(typeof elzero);
 console.log("\`I\'m In \n \\\\ \nLove \\\\ \"\"\" \'\'\'\n+++ With ++ \n\\\"\"\"\\\"\"\" \n\"\"Jvascript\"\"\`\`");
 let a=21 ,b=20;
 console.log(`_${a}_${b}${a}_${b}${a}_${b}${a}_${b}_`);
+// assiegment from 17 to 22
+
+console.log(10 - 20 + 15 * 3 / 190 % 10 - 400); // 0
 
 
+console.log(10 * 20 - 15 + 3 / 190 * 10 - 400); // 0
 
 
+let num = 3;
+
+// Solution One
+console.log(num+num); // 6
+
+// Solution Two
+console.log(++num + true + true); // 6
+
+// Soultion Three
+console.log(--num + --num + true); // 6
+
+// Soultion Four
+console.log(++num + ++num - true); // 6
+
+// Solution Five
+console.log(num + num - true -true); // 6
+
+// Solution Six
+console.log(--num + true + true + true); // 6
+
+let num1 = "10";
+
+// Solution One
+console.log(+num1 + +num1); // 20
+
+// Solution Two
+console.log(-(-num1) -  -num1); // 20
+
+// Solution Three
+console.log(+num1 - -num1); // 20
+
+// Solution Four
+console.log(num1 - -num1); // 20
+
+let points = 10;
+
+// Write Your Code Here
+
+console.log(points + true +true +true); // 13
+
+// Write Your Code Here
+
+console.log(points -true -true); // 8;
+
+//assiegment in numbers 
+
+console.log(100_000); 
+console.log(100000); 
+console.log(5e4 + 5e4); 
+console.log(parseInt("100000")); 
+console.log(parseFloat("100000.OOO")); 
+console.log(Math.round(99999.9)); 
+console.log(Math.trunc(100000.12)); 
+console.log(Math.ceil(99999.9)); 
+console.log(Math.floor(100000.5)); 
+console.log(Math.pow(10,5)); 
+console.log(Math.max(99,100000,888,55)); 
+console.log(Math.min(99999999,100000)); 
+console.log(10000*10); 
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MAX_SAFE_INTEGER - 9007199254740975); 
+
+let myVar = "100.56789 Views";
+
+console.log(parseInt(myVar)); 
+console.log(parseFloat(parseFloat(myVar).toFixed(2))); 
+
+let num2 = 10;
+
+console.log(Number.isInteger(num2) ? num2- 2*true : 2);
+
+let flt = 10.4;
+
+console.log(Math.round(flt)); 
+console.log(Math.floor(flt)); 
+console.log(Math.trunc(flt)); 
+console.log(parseInt(flt)); 
+console.log(parseFloat(flt.toFixed(0))); 
+
+console.log(Math.floor(Math.random() *5)); 
