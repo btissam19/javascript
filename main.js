f816fd4e2f265195f86239449e53fd7b4c52f4d9
Ibtissam <btissam1234btissam12@gmail.com>
2023-07-24 23:45:28 +0100

    oriented object programming in js

--- main.js
+++ main.js
@@ -525,3 +525,163 @@ let numsNew=nums.reduce(function(acc,elt){
 console.log(numsNew)
 // 500
 // [-1, -10, 10, 20, -5, -3]
+
+let numbers = [11, 20, 2, 5, 17, 10];
+
+let evenNumbers = numbers.filter(function (el) {
+  return el % 2 === 0;
+});
+
+console.log(evenNumbers);
+
+
+let addMap = numbers.map(function (el) {
+  return el + el;
+});
+
+console.log(addMap);
+
+let addFilter = numbers.filter(function (el) {
+  return el + el;
+});
+
+console.log(addFilter);
+
+
+console.log(" %coriented programming in js","color:red")
+
+// Create Your Object Here
+let thename="ibtissam",theage=20,thecountry="moroco";
+let member={
+    thename:"ibtissam",
+    theage:20,
+    thecountry:"moroco",
+    thefullDetails: function(){
+        console.log(`MY name is ${thename}MY age is${theage}I live in ${thecountry}`)
+    }
+
+}
+console.log(member.thename); // Elzero
+console.log(member.theage); // 38
+console.log(member. thecountry); // Egypt
+console.log(member.thefullDetails());
+// My Name Is Elzero, My Age Is 38, I Live in Egypt
+
+
+
+// Create Your Object Here
+
+let objMethodOne={
+    property:"Method one"
+}
+console.log(objMethodOne.property); // "Method One"
+
+// Method Two
+// Create Your Object Here
+let objMethodTwo= new Object({ property:"Method tow"})
+console.log(objMethodTwo.property); // "Method Two"
+
+// Method Three
+// Create Your Object Here
+objMethodThree=Object.create({property:"Method Three"})
+console.log(objMethodThree.property); // "Method Three"
+
+// Method Four
+
+let objMethodtest= {property:"Method four"}
+objMethodFour=Object.assign(objMethodtest)
+console.log(objMethodFour.property); // "Method Four"
+
+
+let aa= 1;
+
+let threeNums = {
+  b: 2,
+  c: 3,
+  d: 4,
+};
+
+let twoNums = {
+  e: 5,
+  f: 6,
+};
+
+// Create Your Object Here in One Line
+finalObject=Object.assign({aa},threeNums,twoNums)
+console.log(finalObject);
+
+/*
+  a: 1
+  b: 2
+  c: 3
+  d: 4
+  e: 5
+  f: 6
+*/
+// The Object To Work With
+let myFavGames = {
+    "Trinity Universe": {
+      publisher: "NIS America",
+      price: 40,
+    },
+    "Titan Quest": {
+      publisher: "THQ",
+      bestThree: {
+        one: "Immortal Throne",
+        two: "Ragnarök",
+        three: "Atlantis",
+      },
+      price: 50,
+    },
+    YS: {
+      publisher: "Falcom",
+      bestThree: {
+        one: "Oath in Felghana",
+        two: "Ark Of Napishtim",
+        three: "origin",
+      },
+      price: 40,
+    },
+  };
+  
+  // Code One => How To Get Object Length ?
+  let objectLength =Object.keys(myFavGames).length;
+  console.log(objectLength)
+  
+  for (let i = 0; i < objectLength; i++) {
+    console.log(`The Game Name Is ${myFavGames["Trinity Universe"]}`);
+    console.log(`The Publisher Is  ${myFavGames["Trinity Universe"]}`);
+    console.log(`The Price Is ???????`);}
+  
+//     // Check If Nested Object Has Property (bestThree)
+//     if (???????) {
+//       console.log("- Game Has Releases");
+//       console.log(`First => ???????`);
+//       console.log(`Second => ???????`);
+//       console.log(`Third => ???????`);
+//     }
+//     console.log("#".repeat(20));
+//   }
+  
+  // Ouput
+  
+  "The Game Name Is Trinity Universe"
+  "The Publisher Is NIS America"
+  "The Price Is 40"
+  "####################"
+  "The Game Name Is Titan Quest"
+  "The Publisher Is THQ"
+  "The Price Is 50"
+  "- Game Has Releases"
+  "First => Immortal Throne"
+  "Second => Ragnarök"
+  "Third => Atlantis"
+  "####################"
+  "The Game Name Is YS"
+  "The Publisher Is Falcom"
+  "The Price Is 40"
+  "- Game Has Releases"
+  "First => Oath in Felghana"
+  "Second => Ark Of Napishtim"
+  "Third => origin"
+  "####################"
