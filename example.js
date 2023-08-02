f8c3034c9eb7bdbf30dfbd77ca353ed6eb98902f
Ibtissam <btissam1234btissam12@gmail.com>
2023-08-02 22:06:49 +0100

    cHallenge BOM

---
+++ example.js
@@ -0,0 +1,8 @@
+let numberOne=10 , numberTow=20;
+//for traditional methode we add ""
+console.log(numberOne+""+ numberTow);
+console.log( typeof (numberOne+""+ numberTow));
+//but for new methode we just write variable next to next
+console.log(`${numberOne}${numberTow}`);
+//for traditional way we write \n
+console.log(numberOne+"\n"+ numberTow);
