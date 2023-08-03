b132d636b39257c627fac9f0335d48cdc66bc490
Ibtissam <btissam1234btissam12@gmail.com>
2023-08-03 20:48:43 +0100

    destructing coures

---
+++ firstvide.js
@@ -0,0 +1,21 @@
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+
+let myFriends = ["Ahmed", "Sayed", "Ali", "Maysa"];
+
+[a = "A", b, c, d, e = "Osama"] = myFriends;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+// console.log(myFriends[4]);
+
+let [, y, , z] = myFriends;
+
+console.log(y);
+console.log(z);
